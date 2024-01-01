1882ba5ac5b8a623f109e6e5e780afd770c5f1ba
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-01 16:01:56 +0530

    Successfuly got answer with dummy data

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -40,7 +40,7 @@ fn check_possible_or_not(line: String,conditions :&HashMap<&str,i32>) -> i32{
 
     //game number, in this case 1
     let mut game_number:i32 = match game_line[0].split(" ").collect::<Vec<&str>>()[1].parse(){
-        Err(why) => panic!("Number cant be parsesd"),
+        Err(why) => panic!("{}",why),
         Ok(game_number) => game_number,
     };
 
@@ -50,16 +50,20 @@ fn check_possible_or_not(line: String,conditions :&HashMap<&str,i32>) -> i32{
     //"3 blue,4 red"
     for each_round in game_data.iter(){
         //[" 3 blue"," 4 red"]
-        let each_set:Vec<&str> = each_round.trim().split(",").collect();
+        let each_set:Vec<&str> = each_round.split(",").collect();
         //"3 blue"
         for each_color in each_set.iter(){
             //["3","blue"]
             let each_set_of_color:Vec<&str> = each_color.split(" ").collect();
-            let max_condition_for_this_color = match conditions.get(each_set_of_color[1]){
+            let max_condition_for_this_color = match conditions.get(each_set_of_color[2]){
                 Some(max_condition_for_this_col) => *max_condition_for_this_col,
                 None => panic!(" \nKey not found in conditions,{} & {} & {} \n",each_set_of_color[0],each_set_of_color[1],each_set_of_color[2]),
             };
-            if max_condition_for_this_color < each_set_of_color[0].parse::<i32>().unwrap() {
+            let number_of_balls: i32 = match each_set_of_color[1].parse::<i32>() {
+                Ok(number) => number,
+                Err(why) => panic!("{}", why),
+            };
+            if max_condition_for_this_color < number_of_balls  {
                 return 0;
             }
             
