f9890af62934129f5a8597f542a0f2b053971881
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-01 15:48:35 +0530

    Logically correct, though some errors still persist

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,4 +1,3 @@
-use core::panicking::panic;
 use std::{io::{BufReader, BufRead}
     ,path::Path,
     fs::File};
@@ -24,18 +23,18 @@ fn main(){
     max_conditions.insert("green", 13);
     max_conditions.insert("blue", 14);
 
+    let mut answer = 0;
     for line in reader.lines() {
         let line = match line {
             Err(why) => panic!("Error happend {}",why),
             Ok(line) => line
         };
-        
-
-
+        answer += check_possible_or_not(line, &max_conditions);
     }
+    println!("Answer is {}",answer);
 }
 
-fn check_possible_or_not(line: String,conditions :&HashMap<&str,i32>){
+fn check_possible_or_not(line: String,conditions :&HashMap<&str,i32>) -> i32{
     //["Game 1:", "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"]  the below line has this value
     let game_line: Vec<&str> = line.split(":").collect();
 
@@ -50,17 +49,21 @@ fn check_possible_or_not(line: String,conditions :&HashMap<&str,i32>){
 
     //"3 blue,4 red"
     for each_round in game_data.iter(){
-        //["3 blue","4 red"]
-        let each_set:Vec<&str> = each_round.split(",").collect();
+        //[" 3 blue"," 4 red"]
+        let each_set:Vec<&str> = each_round.trim().split(",").collect();
         //"3 blue"
         for each_color in each_set.iter(){
             //["3","blue"]
             let each_set_of_color:Vec<&str> = each_color.split(" ").collect();
             let max_condition_for_this_color = match conditions.get(each_set_of_color[1]){
-                Ok(max_condition_for_this_col) => max_condition_for_this_col,
-                Err(why) =>panic!("{}",why)
+                Some(max_condition_for_this_col) => *max_condition_for_this_col,
+                None => panic!(" \nKey not found in conditions,{} & {} & {} \n",each_set_of_color[0],each_set_of_color[1],each_set_of_color[2]),
             };
+            if max_condition_for_this_color < each_set_of_color[0].parse::<i32>().unwrap() {
+                return 0;
+            }
             
         }
     }
+    return game_number;
 }
